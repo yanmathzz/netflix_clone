be7133a6835a94ec45d07eaccde575b4689ee579
yan matheus <ymatheus706@gmail.com>
2023-03-10 21:46:48 -0300

    new package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Criando a Netflix
-      {/*header*/}
+     <Header />
       <main>
         {/*banner*/}
         <section>
