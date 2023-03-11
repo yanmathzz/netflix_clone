8d8810f354da5507b1abdcfcb69a3007b09652a8
yan matheus <ymatheus706@gmail.com>
2023-03-11 20:28:59 -0300

    buttons css

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,52 +1,57 @@
-import { useEffect, useState } from 'react'
-import { baseUrl } from '../constants/movie'
-import { Movie } from '../typings'
-import Image from 'next/image'
-import { FaPlay } from 'react-icons/fa'
-import { InformationCircleIcon } from '@heroicons/react/24/solid'
+import { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 interface Props {
-    netflixOriginals: Movie[]
+  netflixOriginals: Movie[];
 }
 
 function Banner({ netflixOriginals }: Props) {
-    const [movie, setMovie] = useState<Movie | null>(null)
+  const [movie, setMovie] = useState<Movie | null>(null);
 
-    useEffect(() => {
-        setMovie(
-            netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
-        )
-    }, [netflixOriginals])
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
 
-    console.log(movie)
+  console.log(movie);
 
-    return (
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end
-        lg:pb-12'>
-            <div className='absolute top-0 left-0 -z-10 h-[95vh] w-screen'>
-                <Image 
-               src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-               layout="fill"
-               objectFit='cover'
-                />
-            </div>
+  return (
+    <div
+      className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end
+        lg:pb-12"
+    >
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
 
-            <h1 className='text-2xl lg:text-7xl md:text-7xl font-bold'>
-            {movie?.title || movie?.name || movie?.original_name}</h1>
-            <p className='max-w-s text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>
-            {movie?.overview}
-            </p>
+      <h1 className="text-2xl lg:text-7xl md:text-7xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-s text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
 
-            <div>
-                <button className='bannerButton bg-white text-black'>
-                    <FaPlay className="h-4 w-4 text-black md:w-7 md:h-7"/>Play
-                    </button>
-                <button className='bannerButton bg-[gray]/70'>
-                    More Info<InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8'/>
-                    </button>
-            </div>
-        </div>
-    )
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:w-7 md:h-7" />
+          Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info
+          <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
