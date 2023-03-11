6786a87c2b4e38b88e488176fbbcaed4b390c0cd
yan matheus <ymatheus706@gmail.com>
2023-03-11 00:32:18 -0300

    function Header Scrolled

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="relative h-screen">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
