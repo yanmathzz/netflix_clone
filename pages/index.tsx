2ce323074125c580ded446636874e88e108703ed
yan matheus <ymatheus706@gmail.com>
2023-03-12 21:25:56 -0300

    html

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,14 +40,14 @@ const Home = ({ netflixOriginals,
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals}/>
         <section className="md:space-y-24">
-        <Row title="Trending Now" movies={trendingNow} />
-          <Row title="Top Rated" movies={topRated} />
-          <Row title="Action Thrillers" movies={actionMovies} />
+        <Row title="Mais Assistidos" movies={trendingNow} />
+          <Row title="Maior Avaliação" movies={topRated} />
+          <Row title="Ação" movies={actionMovies} />
           {/* My List Component */}
-          <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Scary Movies" movies={horrorMovies} />
-          <Row title="Romance Movies" movies={romanceMovies} />
-          <Row title="Documentaries" movies={documentaries} />
+          <Row title="Comédia" movies={comedyMovies} />
+          <Row title="Terror" movies={horrorMovies} />
+          <Row title="Romance" movies={romanceMovies} />
+          <Row title="Documentarios" movies={documentaries} />
         </section>
       </main>
       {showModal && <Modal/>}
