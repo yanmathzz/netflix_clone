bad34ff617e34aebb2744bc115308611c73ddbf8
yan matheus <ymatheus706@gmail.com>
2023-03-13 00:56:31 -0300

    formatacao

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,51 +1,56 @@
 import MuiModal from "@mui/material/Modal";
 import { modalState, movieState } from "../atoms/modalAtoms";
 import { useRecoilState } from "recoil";
-import { PlusIcon, XMarkIcon } from "@heroicons/react/24/solid"
+import { PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
 import { Element, Genre } from "../typings";
-import ReactPlayer from 'react-player/lazy'
-import { FaPlay, FaRegThumbsUp, FaThumbsUp, FaVolumeOff, FaVolumeUp } from "react-icons/fa";
+import ReactPlayer from "react-player/lazy";
+import {
+  FaPlay,
+  FaRegThumbsUp,
+  FaThumbsUp,
+  FaVolumeOff,
+  FaVolumeUp,
+} from "react-icons/fa";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
-  const [movie, setMovie] = useRecoilState(movieState)
-  const [trailer, setTrailer] = useState("")
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [muted, setMuted] = useState(true)
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(true);
 
-
-    useEffect(() => {
-        if (!movie) return 
+  useEffect(() => {
+    if (!movie) return;
 
     async function fetchMovie() {
-        const data = await fetch (
+      const data = await fetch(
         `https://api.themoviedb.org/3/${
-          movie?.media_type === 'tv' ? 'tv' : 'movie'
+          movie?.media_type === "tv" ? "tv" : "movie"
         }/${movie?.id}?api_key=${
           process.env.NEXT_PUBLIC_API_KEY
         }&language=en-US&append_to_response=videos`
-        )
+      )
         .then((response) => response.json())
-        .catch((err) => console.log(err.message))
+        .catch((err) => console.log(err.message));
 
-        if (data?.videos) {
-            const index = data.videos.results.findIndex(
-              (element: Element) => element.type === 'Trailer'
-            )
-            setTrailer(data.videos?.results[index]?.key)
-          }
-          if (data?.genres) {
-            setGenres(data.genres)
-          }
-        }
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
+      }
+      if (data?.genres) {
+        setGenres(data.genres);
+      }
+    }
 
-        fetchMovie()
-    }, [movie])
+    fetchMovie();
+  }, [movie]);
 
   const handleClose = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
   return (
     <MuiModal
@@ -55,84 +60,91 @@ function Modal() {
       overflow-y-scroll rounded-md scrollbar-hide"
     >
       <>
-        <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
-            <XMarkIcon className="h-6 w-6"/>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XMarkIcon className="h-6 w-6" />
         </button>
 
         <div className="relative pt-[56.25%]">
-        <ReactPlayer
+          <ReactPlayer
             url={`https://www.youtube.com/watch?v=${trailer}`}
             width="100%"
             height="100%"
-            style={{ position: 'absolute', top: '0', left: '0' }}
+            style={{ position: "absolute", top: "0", left: "0" }}
             playing
             muted={muted}
           />
           <div className="absolute bottom-10 flex w-full items-center justify-between px-14 ">
             <div className="flex space-x-3">
-                <button className="flex items-center gap-x-5 rounded bg-white px-9
-                text-x3 font-bold text-black transition hover:bg-[#e6e6e6]">
-                <FaPlay className="h-3.5 w-3.5 text-black"/>
+              <button
+                className="flex items-center gap-x-5 rounded bg-white px-9
+                text-x3 font-bold text-black transition hover:bg-[#e6e6e6]"
+              >
+                <FaPlay className="h-3.5 w-3.5 text-black" />
                 Play
-                </button>
+              </button>
 
-                <button className="modalButton">
-                    <PlusIcon className="h-4.5 w-4.5"/>
-                </button>
+              <button className="modalButton">
+                <PlusIcon className="h-4.5 w-4.5" />
+              </button>
 
-                <button className="modalButton">
-                    <FaRegThumbsUp className="h-5 w-5"/>
-                </button>
+              <button className="modalButton">
+                <FaRegThumbsUp className="h-5 w-5" />
+              </button>
             </div>
             <button className="modalButton" onClick={() => setMuted(!muted)}>
-                {muted ? (
-            <FaVolumeOff className="h-4.5 w-4.5 bg-[#181818] hover:bg-[#181818]"/> 
-            ) : (
-            <FaVolumeUp className="h-4.5 w-4.5 bg-[#181818] hover:bg-[#181818]"/>
-            )}
+              {muted ? (
+                <FaVolumeOff className="h-4.5 w-4.5 bg-[#181818] hover:bg-[#181818]" />
+              ) : (
+                <FaVolumeUp className="h-4.5 w-4.5 bg-[#181818] hover:bg-[#181818]" />
+              )}
             </button>
           </div>
         </div>
 
         <div>
-            <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
-                <div className="space-y-6">
-                <div className="flex items-center space-x-2 text-sm">
-                    <p className="font-semibold text-green-400">
-                        {movie!.vote_average * 10 }% Match
-                    </p>
-                    <p className="font-light">
-                        {movie?.release_date || movie?.first_air_date}
-                    </p>
-                    <div className="flex h-4 items-center justify-center rounded border
-                    border-white/40 px-1.5 text-xs">
-                        HD
-                    </div>
+          <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+            <div className="space-y-6">
+              <div className="flex items-center space-x-2 text-sm">
+                <p className="font-semibold text-green-400">
+                  {movie!.vote_average * 10}% Match
+                </p>
+                <p className="font-light">
+                  {movie?.release_date || movie?.first_air_date}
+                </p>
+                <div
+                  className="flex h-4 items-center justify-center rounded border
+                    border-white/40 px-1.5 text-xs"
+                >
+                  HD
                 </div>
+              </div>
 
-                <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
-                    <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+                <p className="w-5/6">{movie?.overview}</p>
                 <div className="flex flex-col space-y-3 text-sm">
-                    <div>
-                        <span className="text-[gray]">Genres: </span>
-                        {genres.map((genre) => genre.name).join(', ')}
-                    </div>
+                  <div>
+                    <span className="text-[gray]">Genres: </span>
+                    {genres.map((genre) => genre.name).join(", ")}
+                  </div>
 
-                    <div>
-                        <span className="text-[gray]">Original language: </span>
-                        {movie?.original_language}
-                    </div>
-                    
-                    <div className="text-[gray]">Total votes: </div>
-                    {movie?.vote_count}
+                  <div>
+                    <span className="text-[gray]">Original language: </span>
+                    {movie?.original_language}
+                  </div>
+
+                  <div className="text-[gray]">Total votes: </div>
+                  {movie?.vote_count}
                 </div>
-                </div>
+              </div>
             </div>
-        </div>
+          </div>
         </div>
       </>
     </MuiModal>
-  )
+  );
 }
 
 export default Modal;
