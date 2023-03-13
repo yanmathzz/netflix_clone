0da09645cc2f7341262edf3cfb567a9596521393
yan matheus <ymatheus706@gmail.com>
2023-03-13 02:51:56 -0300

    api

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,5 +1,5 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY
-const BASE_URL = 'https://api.themoviedb.org/3/'
+const BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
   fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
